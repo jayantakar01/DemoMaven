1623a40bffe564b1db11ad2143e1fe27f4971d97
Jayanta Kar <jayanta.kar01@nagarro.com>
2023-10-10 11:54:06 +0530

    1st commit

---
+++ src/main/java/com/task/jk/App.java
@@ -0,0 +1,13 @@
+package com.task.jk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!,I'm from Dev" );
+    }
+}
